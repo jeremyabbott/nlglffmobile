cbd3789c02ba172ceb4c6f38b4aef83029c6f147
Jeremy Abbott <jeremymabbott@gmail.com>
2015-08-23 20:49:02 -0500

    Adds support for loading sponsors

--- nlglff/Nlglff.fs
+++ nlglff/Nlglff.fs
@@ -4,11 +4,18 @@ open FSharp.Data
 
 let baseApiUrl = "http://nlglff-dev.azurewebsites.net/api/"
 
-
 let filmsUrl = baseApiUrl + "films"
+let sponsorsUrl = baseApiUrl + "sponsors"
 
 type Films = JsonProvider<"http://nlglff-dev.azurewebsites.net/api/films", RootName="Film">
 type Film = Films.Film
 
+type Sponsors = JsonProvider<"http://nlglff-dev.azurewebsites.net/api/sponsors", RootName="Sponsor">
+type Sponsor = Sponsors.Sponsor
+
+
 let loadFilms () =
     Films.Load(filmsUrl)
+
+let loadSponsors () =
+    Sponsors.Load(sponsorsUrl)
