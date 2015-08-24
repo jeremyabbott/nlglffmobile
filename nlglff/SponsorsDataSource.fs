663ad2d46c27b99bc2a7e6c17c038931fc37c0ad
Jeremy Abbott <jeremymabbott@gmail.com>
2015-08-23 20:53:46 -0500

    Adds sponsor data source

---
+++ nlglff/SponsorsDataSource.fs
@@ -0,0 +1,28 @@
+﻿namespace nlglff
+
+open System
+open Foundation
+open Nlglff.Api
+open UIKit
+
+type SponsorsDataSource(sponsorSource : Sponsor array, navigation : UINavigationController) =
+    inherit UITableViewSource()
+
+    let cellIdentifier = "SponsorCell"
+
+    override x.GetCell(view, indexPath) : UITableViewCell =
+        let sponsor = sponsorSource.[int indexPath.Row]
+
+        let cell =
+            match view.DequeueReusableCell cellIdentifier with 
+            | null -> new UITableViewCell(UITableViewCellStyle.Default, cellIdentifier)
+            | cell -> cell
+
+        cell.TextLabel.Text <- sprintf "%s - %s" sponsor.LevelDescription sponsor.DisplayName
+        cell
+
+    override x.RowsInSection(view, section) = nint sponsorSource.Length
+
+    override x.RowSelected (tableView, indexPath) = 
+        tableView.DeselectRow (indexPath, false)
+        //navigation.PushViewController (new FilmDetailViewController(filmSource.[int indexPath.Item]), true)
