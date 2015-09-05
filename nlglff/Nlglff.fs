813e5bddf385d3aec5a413bd184544b4d6fafbc0
Jeremy Abbott <jeremymabbott@gmail.com>
2015-09-05 16:13:50 -0500

    Adds support for aching function results

--- nlglff/Nlglff.fs
+++ nlglff/Nlglff.fs
@@ -1,5 +1,6 @@
 ﻿module Nlglff.Api
 
+open System.Collections.Generic
 open FSharp.Data
 
 let baseApiUrl = "http://nlglff-dev.azurewebsites.net/api/"
@@ -13,9 +14,20 @@ type Film = Films.Film
 type Sponsors = JsonProvider<"http://nlglff-dev.azurewebsites.net/api/sponsors", RootName="Sponsor">
 type Sponsor = Sponsors.Sponsor
 
+let memoize name f =
+    let dict = new System.Collections.Generic.Dictionary<_,_>()
 
-let loadFilms () =
-    Films.Load(filmsUrl)
+    fun () ->
+        match dict.TryGetValue(name) with
+        | (true, v) -> v
+        | _ ->
+            let temp = f()
+            dict.Add(name, temp)
+            temp
 
-let loadSponsors () =
-    Sponsors.Load(sponsorsUrl)
+let loadFilms = memoize "films" (fun () ->
+    Films.Load(filmsUrl))
+
+
+let loadSponsors = memoize "sponsors" (fun () ->
+    Sponsors.Load(sponsorsUrl))
